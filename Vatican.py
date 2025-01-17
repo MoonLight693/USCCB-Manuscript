0cbcea36aadb927ff428a6ebac010c246426f79d
MoonLight693 <whitmercraft939@gmail.com>
2025-01-17 15:14:13 -0500

    nuget Vatican script

---
+++ Vatican.py
@@ -0,0 +1,9 @@
+'''
+Author: Evan Whitmer
+Last Date Modified: January 17, 2025
+Description: This script uses wget command to bulk download the 
+Vatican Catechism at https://www.vatican.va/archive/ENG0015/_INDEX.HTM. 
+Then, parses the Paragraph and its number into a usable table.
+Links to additional resources:
+'''
+
