d743b9aa4ac05aad8e6ea61c49c3a0548dad261c
MoonLight693 <whitmercraft939@gmail.com>
2025-01-19 11:49:49 -0500

    made a testing folder and can now pull paragraphs in the right format

--- Vatican.py
+++ Vatican.py
@@ -22,6 +22,19 @@ tree = etree.parse(StringIO(str(response.text)), parser)
 # define a specific element in the tree to wish to grab
 paragraph = tree.xpath('//p[@class="MsoNormal"]/text()')
 
+# remove the string gargen : \n\r, \n, footnote markers
 for i in range(len(paragraph)):
     paragraph[i] = " ".join(paragraph[i].splitlines())
-    print(paragraph[i])
+
+# stitch the paragraphs together so each element is 1 CCC paragraph
+x = len(paragraph)
+i=0
+while i < x:
+    if not paragraph[i][0].isdigit() and i != 0:
+        # if the start of the string is not the CCC number and is not the first paragraph in the list
+        b = [''.join(paragraph[i-1:i+1])]  # join the current and last elements
+        paragraph = paragraph[:i-1] + b + paragraph[i+1:] # reconstruct the list with the stitch and skip
+        x = x-1 # decrease the count of length of list
+        i += 1 # move to next element
+    else: i += 1 # element contatins CCC number, so do nothing
+print(paragraph)
