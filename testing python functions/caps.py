14d66c88a349b198f5d300b3a712260257b3bcb6
MoonLight693 <whitmercraft939@gmail.com>
2025-01-23 11:11:58 -0500

    comments on testings

--- testing python functions/caps.py
+++ testing python functions/caps.py
@@ -1,3 +1,9 @@
+'''
+Author: Evan Whitmer
+Last Date Modified: January 23, 2025
+Description: testing for removing strings of all capitalized letters.
+Links to additional resources:
+'''
 listy = ['PART ONE:', 'We begin our profession of faith by saying: "I believe" or "We believe". Before expounding the Church\'s faith, as confessed in the Creed, celebrated in the liturgy and lived in observance of God\'s commandments and in prayer, we must first ask what "to believe" means. Faith is man\'s response to God, who reveals himself and gives himself to man, at the same time bringing man a superabundant light as he searches for the ultimate meaning of his life. Thus we shall consider first that search (Chapter One), then the divine Revelation by which God comes to meet man (Chapter Two), and finally the response of faith (Chapter Three).']
 
 for item in listy:
--- testing python functions/quotetation.py
+++ testing python functions/quotetation.py
@@ -1,3 +1,11 @@
+'''
+Author: Evan Whitmer
+Last Date Modified: January 23, 2025
+Description: replace "" with " as error fixing in grep ing paragraphs.
+Links to additional resources:
+- https://stackoverflow.com/questions/70463905/replace-method-not-working-over-single-quote
+- https://www.geeksforgeeks.org/python-remove-substring-list-from-string/
+'''
 my_list = ["a\"\"ppl\"\"e", "banana", "cherry", '"We begin our profession of faith by saying: ""I believe"" or ""We believe""']
 
 # Replace "a" with "o" in each string
