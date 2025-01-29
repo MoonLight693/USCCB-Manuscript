381903186e8b2c576bd2c6ab6c2478081ab433cf
Josh Atkinson <jatkinson001@student.franciscan.edu>
2025-01-29 13:20:14 -0500

    state machine

---
+++ State Machine/State_Machine.py
@@ -0,0 +1,12 @@
+'''
+Author: Josh Atkinosn 
+Last Date Modified: January 27, 2025
+Description: This script will handle all manuscript quotations 
+Vatican Catechism at https://www.vatican.va/archive/ENG0015/_INDEX.HTM. 
+Then, parses the Paragraph and its number into a usable table.
+Links to additional resources:
+'''
+
+#takes in manuscripts output quotes in a list
+def manuscript(text):
+    return
