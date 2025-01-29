6834521a0330a9a2a526787847c2ed6145ea09dd
chrisbarreras <chris.barreras@yahoo.com>
2025-01-29 14:29:02 -0500

    Fixed PDFConverter

--- convert_pdfs_to_text.py
+++ convert_pdfs_to_text.py
@@ -1,18 +1,16 @@
 import os
-from PyPDF2 import PdfReader
+import pdfplumber
 
 def convert_pdfs_to_text(input_dir, output_dir):
     for filename in os.listdir(input_dir):
         filepath = os.path.join(input_dir, filename)
-        with open(filepath, 'rb') as pdf_file:
-            reader = PdfReader(pdf_file)
+        with pdfplumber.open(filepath) as pdf:
             text = ""
-            for page_num in range(len(reader.pages)):
-                page = reader.pages[page_num]
+            for page in pdf.pages:
                 text += page.extract_text()
         output_filename = os.path.splitext(filename)[0] + ".txt"
         output_path = os.path.join(output_dir, output_filename)
         with open(output_path, 'w') as text_file:
             text_file.write(text)
-            
-#convert_pdfs_to_text("USCCB Test Input Directory", "USCCB Test Output Directory")
+
+convert_pdfs_to_text("USCCB Test Input Directory", "USCCB Test Output Directory")
