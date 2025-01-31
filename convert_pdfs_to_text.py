f7d669b562d75e097fcac523102a0d5431622e5e
chrisbarreras <chris.barreras@yahoo.com>
2025-01-31 13:58:58 -0500

    updated pdfconverter

--- convert_pdfs_to_text.py
+++ convert_pdfs_to_text.py
@@ -7,10 +7,10 @@ def convert_pdfs_to_text(input_dir, output_dir):
         with pdfplumber.open(filepath) as pdf:
             text = ""
             for page in pdf.pages:
-                text += page.extract_text()
+                text += page.extract_text() or ""
         output_filename = os.path.splitext(filename)[0] + ".txt"
         output_path = os.path.join(output_dir, output_filename)
-        with open(output_path, 'w') as text_file:
+        with open(output_path, 'w', encoding='utf-8') as text_file:
             text_file.write(text)
 
-#convert_pdfs_to_text("USCCB Test Input Directory", "USCCB Test Output Directory")
+# convert_pdfs_to_text("USCCB Test Input Directory", "USCCB Test Output Directory")
