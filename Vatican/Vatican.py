94b409a66f1bbf584d1f9fc05da66b64b30d306c
Evan Whitmer <whitmercraft939@gmail.com>
2025-02-13 02:24:29 -0500

    on page P16 of adding to the ban list

--- Vatican/Vatican.py
+++ Vatican/Vatican.py
@@ -40,7 +40,18 @@ def stitching(paragraph):
     ''' stitch the paragraphs together so each element is one CCC paragraph '''
     
     banned = ['Part One: the Profession of Faith','Part Two: the Sacraments of Faith', 'Part Three: the Life of Faith',
-              'Part Four: Prayer in the Life of Faith', 'Above all - Charity']
+              'Part Four: Prayer in the Life of Faith', 'Above all - Charity', 'The covenant with Noah', 'God chooses Abraham',
+              'God forms his people Israel', 'God has said everything in his Word', 'There will be no further Revelation',
+              '. . . continued in apostolic succession', 'One common source. . .', '. . . two distinct modes of transmission',
+              'Apostolic Tradition and ecclesial traditions', 'The heritage of faith entrusted to the whole of the Church',
+              'The Magisterium of the Church', 'The dogmas of the faith', 'The supernatural sense of faith', 'Growth in understanding the faith',
+              'The Old Testament', 'The New Testament', 'The unity of the Old and New Testaments', 'Abraham - "father of all who believe"',
+              'Mary - "Blessed is she who believed"', 'To believe in God alone', 'To believe in Jesus Christ, the Son of God',
+              'To believe in the Holy Spirit', 'Faith is a grace', 'Faith is a human act', 'Faith and understanding', 'The freedom of faith',
+              'The necessity of faith', 'Perseverance in faith', 'Faith - the beginning of eternal life', 'I. "I BELIEVE IN ONE GOD"',
+              'II. GOD REVEALS HIS NAME', 'The living God', '"I Am who I Am"', '"A God merciful and gracious"', 'God alone IS',
+              'III. GOD, "HE WHO IS", IS TRUTH AND LOVE', 'God is Truth', 'God is Love', 'IV. THE IMPLICATIONS OF FAITH IN ONE GOD',
+              'IN BRIEF']
     
     x = len(paragraph) - 1
     i=0
@@ -49,6 +60,7 @@ def stitching(paragraph):
         '''This loop removes banned phrases/subheaders that aren't paragraphs.'''
         if paragraph[i] in banned:
             #print(f"popped: {paragraph.pop(i)}")
+            paragraph.pop(i)
             x-=1
         else: i+=1
     
@@ -66,7 +78,7 @@ def stitching(paragraph):
             b = [''.join(paragraph[i-1:i+1])]  # join the current and last elements
             paragraph = paragraph[:i-1] + b + paragraph[i+1:] # reconstruct the list with the stitch and skip
             x = x-1 # decrease the count of length of list
-            #i += 1 # move to next element
+            #todo: removed and fixed the process of stitching. i += 1 # move to next element
         else: i += 1 # element contains CCC number, so do nothing
 
     # fail safe check for if the last item in the paragraph list was skipped in stitching
@@ -139,11 +151,18 @@ f = open(file_path, "w")
 for p in paragraph: f.write(p + "\n")
 f.close()
 
-page = "PA"
-while page != "PB":
+page = "P10"
+while page != "P20":
     paragraph = parsing("https://www.vatican.va/archive/ENG0015/__" + page + ".HTM")
     #print(paragraph)
-    paragraph = stitching(paragraph)
+    
+    '''logic around the page with the Creeds on them.'''
+    if page == "P13":
+        paragraph = ['-4 The Apostles Creed I believe in God the Father almighty, creator of heaven and earth. I believe in Jesus Christ, his only Son, our Lord. He was conceived by the power of the Holy Spirit and born of the Virgin Mary Under Pontius Pilate He was crucified, died, and was buried. He descended to the dead. On the third day he rose again. He ascended into heaven and is seated at the right hand of the Father. He will come again to judge the living and the dead. I believe in the Holy Spirit, the holy catholic Church, the communion of saints, the forgiveness of sins, the resurrection of the body, and the life everlasting. Amen.' ,
+                     '-5 The Nicene Creed We believe in one God, the Father, the Almighty, maker of heaven and earth, of all that is, seen and unseen. We believe in one Lord, Jesus Christ, the only Son of God, eternally begotten of the Father, God from God, Light from Light, true God from true God, begotten, not made, of one Being with the Father. Through him all things were made. For us men and for our salvation, he came down from heaven: by the power of the Holy Spirit he was born of the Virgin Mary, and became man. For our sake he was crucified under Pontius Pilate; he suffered died and was buried. On the third day he rose again in fulfillment of the Scriptures; he ascended into heaven and is seated at the right hand of the Father. He will come again in glory to judge the living and the dead, and his kingdom will have no end. We believe in the Holy Spirit, the Lord, the giver of life, who proceeds from the Father and the Son. With the Father and the Son he is worshipped and glorified. He has spoken through the Prophets. We believe in one holy catholic and apostolic Church. We acknowledge one baptism for the forgiveness of sins. We look for the resurrection of the dead, and the life of the world to come. Amen.']
+    else: 
+        paragraph = stitching(paragraph)
+    
     #print(paragraph)
     appending(paragraph, file_path)
     page = page_next(page)
