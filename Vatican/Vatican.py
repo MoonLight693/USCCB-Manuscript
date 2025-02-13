7903e263cb0ea4947cc90cea4c13432e592d5132
MoonLight693 <59546350+MoonLight693@users.noreply.github.com>
2025-02-13 04:32:01 -0500

    added to the ban list up to P1Z, fixed duplication, fixed missing italicized/brief paragraphs

--- Vatican/Vatican.py
+++ Vatican/Vatican.py
@@ -16,6 +16,7 @@ from io import StringIO
 import os
 
 def parsing(URL):
+    '''Used to grab paragraphs from Vatican catechism website. If the tree returns blank, autocheckes for italicized texted instead.'''
     # define a parsing method for gathering elements of the html
     parser = etree.HTMLParser()
 
@@ -25,10 +26,25 @@ def parsing(URL):
     tree = etree.parse(StringIO(str(response.text)), parser)
     # define a specific element in the tree to wish to grab
     paragraph = tree.xpath('//p[@class="MsoNormal"]/text()')
-    
-    if not paragraph:
-        # if paragpraph empty, then your at a brief page in CCC
-        paragraph = tree.xpath('//p[@class="MsoNormal"]/i/text()')
+
+    # remove the string gargen : \n\r, \n, footnote markers
+    for i in range(len(paragraph)):
+        paragraph[i] = " ".join(paragraph[i].splitlines())
+        
+    return paragraph
+
+def parsingI(URL):
+    '''used to grab the italicize paragraph. Hot fix for pages that have normal and italicized text.'''
+    # define a parsing method for gathering elements of the html
+    parser = etree.HTMLParser()
+
+    # get the web page html
+    response = requests.get(URL)
+    # all html lines can be accessed via the tree by parsing
+    tree = etree.parse(StringIO(str(response.text)), parser)
+
+    # grabs italicize paragraph, then your at a brief page in CCC
+    paragraph = tree.xpath('//p[@class="MsoNormal"]/i/text()')
 
     # remove the string gargen : \n\r, \n, footnote markers
     for i in range(len(paragraph)):
@@ -51,7 +67,40 @@ def stitching(paragraph):
               'The necessity of faith', 'Perseverance in faith', 'Faith - the beginning of eternal life', 'I. "I BELIEVE IN ONE GOD"',
               'II. GOD REVEALS HIS NAME', 'The living God', '"I Am who I Am"', '"A God merciful and gracious"', 'God alone IS',
               'III. GOD, "HE WHO IS", IS TRUTH AND LOVE', 'God is Truth', 'God is Love', 'IV. THE IMPLICATIONS OF FAITH IN ONE GOD',
-              'IN BRIEF']
+              'IN BRIEF', 'I. "IN THE NAME OF THE FATHER AND OF THE SON AND OF THE HOLY SPIRIT"', 'II. THE REVELATION OF GOD AS TRINITY',
+              'The Father revealed by the Son', 'The Father and the son revealed by the spirit', 'III. THE HOLY TRINITY IN THE TEACHING OF THE FAITH',
+              'The formation of the Trinitarian dogma', 'The dogma of the Holy Trinity', 'IV. THE DIVINE WORKS AND THE TRINITARIAN MISSIONS'
+              'IN BRIEF', "The mystery of God's apparent powerlessness", 'IN BRIEF', 'I. CATECHESIS ON CREATION', 'II. CREATION - WORK OF THE HOLY TRINITY',
+              'III. "THE WORLD WAS CREATED FOR THE GLORY OF GOD"', 'IV. THE MYSTERY OF CREATION', 'God creates by wisdom and love',
+              'God creates an ordered and good world', 'God transcends creation and is present to it', 'God upholds and sustains creation', 
+              'V. GOD CARRIES OUT HIS PLAN: DIVINE PROVIDENCE', 'Providence and secondary causes', 'Providence and the scandal of evil', 'IN BRIEF',
+              'I. THE ANGELS', 'The existence of angels - a truth of faith', 'Who are they?', 'Christ "with all his angels"', 'The angels in the life of the Church',
+              'II. THE VISIBLE WORLD', 'IN BRIEF', 'I. "IN THE IMAGE OF GOD"', 'II. "BODY AND SOUL BUT TRULY ONE"', 'III. "MALE AND FEMALE HE CREATED THEM"',
+              'Equality and difference willed by God', '"Each for the other" - "A unity in two"', 'IV. MAN IN PARADISE', 'IN BRIEF',
+              'I. WHERE SIN ABOUNDED, GRACE ABOUNDED ALL THE MORE', 'The reality of sin', 'Original sin - an essential truth of the faith',
+              'How to read the account of the fall', 'II. THE FALL OF THE ANGELS', 'III. ORIGINAL SIN', 'Freedom put to the test', "Man's first sin",
+              "The consequences of Adam's sin for humanity", 'A hard battle. . .', 'IV. "YOU DID NOT ABANDON HIM TO THE POWER OF DEATH"', 'IN BRIEF',
+              'The Good News: God has sent his Son', 'At the heart of catechesis: Christ', 'I. WHY DID THE WORD BECOME FLESH?', 'II. THE INCARNATION',
+              'III. TRUE GOD AND TRUE MAN', 'IV. HOW IS THE SON OF GOD MAN?', "Christ's soul and his human knowledge", "Christ's human will",
+              "Christ's true body", 'The heart of the Incarnate Word', 'IN BRIEF', 'I. CONCEIVED BY THE POWER OF THE HOLY SPIRIT. . .',
+              'II.... BORN OF THE VIRGIN MARY', "Mary's predestination", 'The Immaculate Conception', "Mary's divine motherhood", 
+              "Mary's virginity", 'Mary - "ever-virgin"', "Mary's virginal motherhood in God's plan", 'IN BRIEF', "I. CHRIST'S WHOLE LIFE IS MYSTERY",
+              "Characteristics common to Jesus' mysteries", 'Our communion in the mysteries of Jesus', "II. THE MYSTERIES OF JESUS' INFANCY AND HIDDEN LIFE",
+              'The preparations', 'The Christmas mystery', "The mysteries of Jesus' infancy", "The mysteries of Jesus' hidden life",
+              "III. THE MYSTERIES OF JESUS' PUBLIC LIFE", 'The baptism of Jesus', "Jesus' temptations", '"The kingdom of God is at hand"',
+              'The proclamation of the kingdom of God', 'The signs of the kingdom of God', '"The keys of the kingdom"', 'A foretaste of the kingdom: the Transfiguration',
+              "Jesus' ascent to Jerusalem", "Jesus' messianic entrance into Jerusalem", 'IN BRIEF', 'I. JESUS AND THE LAW', 'II. JESUS AND THE TEMPLE',
+              "III. JESUS AND ISRAEL'S FAITH IN THE ONE GOD AND SAVIOUR", 'IN BRIEF', 'I. THE TRIAL OF JESUS', 'Divisions among the Jewish authorities concerning Jesus',
+              "Jews are not collectively responsible for Jesus' death", "All sinners were the authors of Christ's Passion", "II. CHRIST'S REDEMPTIVE DEATH IN GOD'S PLAN OF SALVATION",
+              '"Jesus handed over according to the definite plan of God"', '"For our sake God made him to be sin"', 'God takes the initiative of universal redeeming love',
+              'III. CHRIST OFFERED HIMSELF TO HIS FATHER FOR OUR SINS', "Christ's whole life is an offering to the Father", '"The Lamb who takes away the sin of the world"',
+              "Jesus freely embraced the Father's redeeming love", 'At the Last Supper Jesus anticipated the free offering of his life', 'The agony at Gethsemani',
+              "Christ's death is the unique and definitive sacrifice", 'Jesus substitutes his obedience for our disobedience', 'Jesus consummates his sacrifice on the cross',
+              "Our participation in Christ's sacrifice", 'IN BRIEF', 'Christ in the tomb in his body', '"You will not let your Holy One see corruption"',
+              '"Buried with Christ. . ."', 'IN BRIEF', 'IN BRIEF', 'I. THE HISTORICAL AND TRANSCENDENT EVENT', 'The empty tomb', 'The appearances of the Risen One',
+              "The condition of Christ's risen humanity", 'The Resurrection as transcendent event', 'II. THE RESURRECTION - A WORK OF THE HOLY TRINITY',
+              'III. THE MEANING AND SAVING SIGNIFICANCE OF THE RESURRECTION', 'IN BRIEF', 'Christ already reigns through the Church. . .'
+              '. . . until all things are subjected to him', 'The glorious advent of Christ, the hope of Israel', "The Church's ultimate trial"]
     
     x = len(paragraph) - 1
     i=0
@@ -82,7 +131,7 @@ def stitching(paragraph):
         else: i += 1 # element contains CCC number, so do nothing
 
     # fail safe check for if the last item in the paragraph list was skipped in stitching
-    if not paragraph[-1][0].isdigit():
+    if paragraph and not paragraph[-1][0].isdigit():
         b = [''.join(paragraph[-2:])]
         paragraph = paragraph[:-2] + b 
     
@@ -165,4 +214,11 @@ while page != "P20":
     
     #print(paragraph)
     appending(paragraph, file_path)
+    
+    '''parses with '''
+    paragraph = parsingI("https://www.vatican.va/archive/ENG0015/__" + page + ".HTM")
+    if paragraph:
+        paragraph = stitching(paragraph)
+        appending(paragraph, file_path)
+    
     page = page_next(page)
